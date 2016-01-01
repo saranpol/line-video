201b9bd0b038fcdba0a4e05b7b836848d2127c72
saranpol <saran@hlpth.com>
2016-01-02 04:04:43 +0700

    asdf

--- server/js/.module-cache/0d1c1ca78362aed025559cf2050c260559803220.js
+++
@@ -1,8 +0,0 @@
-var Nav = React.createClass({displayName: "Nav",
-    render: function() {
-        var p = this.props;
-        return (
-            React.createElement("label", {className: "checkbox-inline"}, React.createElement("input", {type: "checkbox"}), p.title)
-        );
-    }
-});
--- server/js/.module-cache/aa41e322ffb72420fc932b69527b9afe23de1bd7.js
+++
@@ -1,8 +0,0 @@
-var NavBar = React.createClass({displayName: "NavBar",
-    render: function() {
-        var p = this.props;
-        return (
-            React.createElement("label", {className: "checkbox-inline"}, React.createElement("input", {type: "checkbox"}), p.title)
-        );
-    }
-});
